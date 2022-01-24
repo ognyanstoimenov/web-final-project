8f384b27ba1bcf081d41525da8d77a11efcfcace
NVakarelski <72190157+NVakarelski@users.noreply.github.com>
2022-01-24 18:28:08 +0200

    initial version of file parser and objects-User,Lecture (#5)

---
+++ lecture.php
@@ -0,0 +1,19 @@
+<?php
+include 'users.php';
+class Lecture {
+    protected $presentPeople  = [];
+
+    function __construct(array $users ) {
+        foreach($users as $row => $data)
+        {
+            $this->presentPeople[] = $data;
+        }
+        
+    }   
+
+    function getAttendance(){
+        return $presentPeople;
+    }
+
+}
+?>
