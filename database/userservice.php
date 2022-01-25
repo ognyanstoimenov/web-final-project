00d06154cc67917d6ae740ab2db33f6d3e3973c7
Ognyan Stoimenov <ognyan.stoimenov@gmail.com>
2022-01-26 00:08:23 +0200

    Create courses and lectures in database and add login functionality

--- database/userservice.php
+++ database/userservice.php
@@ -1,29 +1,32 @@
 <?php
 class UserService {
-    private $db;
+    private Db $db;
 
     public function __construct($db) {
         $this->db = $db;
     }
 
-    public function registerUser($email, $password) {
-        $conn = $this->db->getConnection();
+    public function registerUser($email, $password) : bool {
         $userTable = $this->db->getUserTableName();
         $sql = "INSERT INTO $userTable (email, password) VALUES (?, ?)";
+        return $this->executeSql($sql, $email, $password);
+    }
+
+    public function loginUser($email, $password) : bool {
+        $userTable = $this->db->getUserTableName();
+        $sql = "SELECT * FROM $userTable WHERE email=? and password=?";
+        return $this->executeSql($sql, $email, $password);
+    }
+
+    private function executeSql($sql, $email, $password) : bool {
+        $conn = $this->db->getConnection();
         $hash = sha1($password);
         $prep = $conn->prepare($sql);
         try {
             return $prep->execute(array($email, $hash));
         }
-        catch (PDOException $e) {
-
+        catch (PDOException) {
+            return false;
         }
     }
-
-    public function loginUser($email, $password)
-    {
-        # todo;
-    }
 }
-
-?>
